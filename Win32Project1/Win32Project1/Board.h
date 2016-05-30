d6e19251f47f3a4d458334229a2bd862ff8cb671
7shevan <shevanussbaum@gmail.com>
2016-05-30 18:12:39 +0300

    Board.h change

--- Win32Project1/Win32Project1/Board.h
+++ Win32Project1/Win32Project1/Board.h
@@ -1,22 +1,94 @@
 #pragma once
 #include "Square.h"
+#include "Integer.h"
+#include "Cos.h"
+#include "Ex.h"
+#include "Fraction.h"
+#include "Function.h"
+#include "Joker.h"
+#include "Log.h"
+#include "Sin.h"
+#include "Tan.h"
+#include "SquareRoot.h"
 
-template <class size, class seq>
+#include <stdlib.h>     /* srand, rand */
+#include <time.h>       /* time */
+#include<vector>
+#include <iostream>
+
+//            0      1         2       3         4          5       6      7       8        9
+enum squares { joker, function, fraction, integer, squareroot, logorithm, sinus, tangens, cosinus, exponential };
+
+template <int size>
 class Board
 {
 public:
 	Board();
 	~Board();
 private:
-	Square* std::array[size][size] m_squares;
+	std::vector<std::vector<Square*>> m_array;
+	int m_range;
 };
 
-template<class size, class seq>b
-inline Board<size, seq>::Board()
+
+template<int size>
+inline Board<size>::Board()
 {
+	//should randomly build all different types
+	m_array.resize(size);
+	m_range = size*size;
+	srand(time(NULL));
+
+	for (int i = 0; i < size; i++)
+	{
+		for (int j = 0; j < size; j++) {
+			switch (rand() % 5)
+			{
+			case joker:m_array[i].emplace_back(new Joker); break;
+			case function:
+			{
+				double num = rand() % m_range;
+				switch (rand() % 5 + 5) //choose one of the fucntions
+				{
+				case exponential:m_array[i].emplace_back(new Ex(num)); break;
+				case logorithm:m_array[i].emplace_back(new Log(num)); break;
+				case sinus:m_array[i].emplace_back(new Sin(num)); break;
+				case cosinus:m_array[i].emplace_back(new Cos(num)); break;
+				case tangens:m_array[i].emplace_back(new Tan(num)); break;
+				}
+			}break;
+			case squareroot: {
+				int num = rand() % m_range / 2;
+				m_array[i].emplace_back(new SquareRoot(sqrt(num))); //this way its defientely a number whose square root is integer
+				break;
+			}
+			case integer:
+			{
+				m_array[i].emplace_back(new Integer(rand() % m_range));
+				break;
+			}
+			case fraction:m_array[i].emplace_back(new Fraction(rand() % m_range, rand() % m_range)); break;
+			}
+			//std::cout << m_array[i][j]->getEnd() << " ";
+
+
+		}
+	}
+
+
+	for (int i = 0; i < size; i++)
+	{
+		for (int j = 0; j < size; j++) {
+			m_array[i][j]->show();
+			std::cout << " ";
+		}
+
+		std::cout << std::endl;
+	}
+
 }
 
-template<class size, class seq>
-inline Board<size, seq>::~Board()
+template<int size>
+inline Board<size>::~Board()
 {
 }
